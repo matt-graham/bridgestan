1cfc4f04612aa44eef95cc45d714cdf2914b10d8
Matt Graham <matthew.m.graham@gmail.com>
2026-05-28 18:54:47 +0100

    Change from tinystan to bridgestan exports

--- wasm/build/local.mk
+++ wasm/build/local.mk
@@ -13,6 +13,6 @@ CXXFLAGS+=-fwasm-exceptions
 
 LDFLAGS+=-sMODULARIZE -sEXPORT_NAME=createModule -sEXPORT_ES6 -sENVIRONMENT=web,worker -sINCOMING_MODULE_JS_API=print,printErr
 LDFLAGS+=-sEXIT_RUNTIME=1 -sALLOW_MEMORY_GROWTH=1
-# Functions we want. Can add more, with a prepended _, from tinystan.h
-EXPORTS=_malloc,_free,_tinystan_api_version,_tinystan_create_model,_tinystan_destroy_error,_tinystan_destroy_model,_tinystan_get_error_message,_tinystan_get_error_type,_tinystan_model_num_free_params,_tinystan_model_param_names,_tinystan_sample,_tinystan_separator_char,_tinystan_stan_version
+# Functions we want. Can add more, with a prepended _, from bridgestan.h
+EXPORTS=_malloc,_free,_bs_model_construct,_bs_model_destruct,_bs_free_error_msg,_bs_name,_bs_model_info,_bs_param_names,_bs_param_unc_names,_bs_param_num,_bs_param_unc_num,_bs_param_constrain,_bs_param_unconstrain,_bs_param_unconstrain_json,_bs_log_density,_bs_log_density_gradient,_bs_log_density_hessian,_bs_log_density_hessian_vector_product,_bs_rng_construct,_bs_rng_destruct,_bs_set_print_callback
 LDFLAGS+=-sEXPORTED_FUNCTIONS=$(EXPORTS) -sEXPORTED_RUNTIME_METHODS=stringToUTF8,getValue,UTF8ToString,lengthBytesUTF8,HEAPF64
